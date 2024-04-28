c6feea805b85778355c8916dd7b4334ff48dce21
Jhon <49782541+en3jott@users.noreply.github.com>
2024-04-28 16:43:10 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// document.addEventListener("DOMContentLoaded", function(event) {
+//     let hacker = document.querySelector('.promo__bg-up');
+    
+//     document.onmousemove = (e)=>{
+//         let x = e.clientX / window.innerWidth;
+//         let y = e.clientY / window.innerHeight;
+    
+//         hacker.style.transform = "translate(-" + x * 70 + "px, -" + y * 70 + "px)";
+//     };
+// })
